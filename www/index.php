38200bccba2ff5f07bab93fb254317f48925ec8f
Andrei Timotin <andreitimotin11@gmail.com>
2017-07-24 23:44:27 +0300

    testing

--- www/index.php
+++ www/index.php
@@ -35,8 +35,12 @@
 //	var_dump($a);
 	var_dump( 3 / 1);
 	var_dump(1 / 3);
-	var_dump('20cats' + 40);
+//	var_dump('20cats' + 40);
 	var_dump(18 % 4);
+
+    echo ($a = 2);
+    $x = ($y = 12) - 8;
+    echo $x;
 ?>
 
 </body>
