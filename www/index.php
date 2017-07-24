aabe8dbc292a337056aadf08c7243dbbd0342815
Andrei Timotin <andreitimotin11@gmail.com>
2017-07-24 23:57:54 +0300

    testing

--- www/index.php
+++ www/index.php
@@ -31,27 +31,8 @@
 <body>
 
 <?php
-//	$a = (4 == 2);
-//	var_dump($a);
-	var_dump( 3 / 1);
-	var_dump(1 / 3);
-//	var_dump('20cats' + 40);
-	var_dump(18 % 4);
 
-    echo '<br>';
-    echo ($a = 2);echo '<br>';
-    $x = ($y = 12) - 8;
-    echo $x; echo '<br>';
-
-
-
-
-
-    var_dump(1 == 1.0);
-    var_dump(1 === 1.0);
-    var_dump('02' == 2);
-    var_dump('02' === 2);
-    var_dump('02' == '2');
+    
 ?>
 
 </body>
